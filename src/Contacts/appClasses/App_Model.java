94e804e5870e052a71eeff2e6f957bce8add51ff
gosi333 <gowsigan.gowribalan@students.fhnw.ch>
2021-04-06 11:21:25 +0200

    Im Model readFile & readContact hinzugefügt

--- src/Contacts/appClasses/App_Model.java
+++ src/Contacts/appClasses/App_Model.java
@@ -1,5 +1,11 @@
 package Contacts.appClasses;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 
 import Contacts.ServiceLocator;
@@ -13,19 +19,50 @@ import Contacts.abstractClasses.Model;
  * @author Brad Richards
  */
 public class App_Model extends Model {
+	private static String CONTACT_FILE = "contact.txt";
+	private static String SEPARATOR = ";"; // Separator for "split"
 	
 	ServiceLocator serviceLocator;
-	private static ArrayList<Contact> myContacts;
+	private ArrayList<Contact> myContacts;
+	
+
     private int value;
     
     public App_Model() {
-        value = 0;
-        
         serviceLocator = ServiceLocator.getServiceLocator();        
         serviceLocator.getLogger().info("Application model initialized");
+        
+       
     }
     
-    public int getValue() {
+    public void readFile() {
+    	File contactFile = new File(CONTACT_FILE);
+    	try(Reader inReader = new FileReader(contactFile)) {
+    		BufferedReader fileIn = new BufferedReader(inReader);
+    		myContacts = new ArrayList<Contact>(); // Liste wird erstellt
+    		String line = fileIn.readLine(); // Zeile lesen
+    		while(line != null) {
+    			Contact contact = readContact(line);
+    			myContacts.add(contact);
+    			line = fileIn.readLine();
+    		}
+    	} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    private Contact readContact(String line) {
+		String [] attributes = line.split(SEPARATOR);
+		String vName = attributes[0];
+		String nName = attributes[1];
+		String eMail = attributes[2];
+		int phoneNumber = Integer.parseInt(attributes[3]);
+		Contact contact = new Contact(vName, nName, eMail, phoneNumber);
+		return contact;
+	}
+
+	public int getValue() {
         return value;
     }
     
